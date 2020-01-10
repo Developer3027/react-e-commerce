d7cbd90da08dafab72503057f3ee672ab41825d4
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-01-10 16:19:00 -0500

    added header and links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -9,12 +10,13 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
+      <Header />
+        <Switch>
         
-        <Route path='/shop' component={ShopPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
         
-      </Switch>
+        </Switch>
     </div>
   );
 }
